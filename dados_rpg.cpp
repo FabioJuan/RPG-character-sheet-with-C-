422a6d75aa94dc947268a3cc418f3d406d780a99
FabioJuan <judoca041@gmail.com>
2026-01-30 14:36:30 -0300

    modulando o progama e o novo vetor

--- dados_rpg.cpp
+++ dados_rpg.cpp
@@ -1,58 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include "pstatus.hpp"
+#include "vetor.hpp"
 
 using namespace std;
-
-status padrao(status Spadrao){
-    Spadrao.hp = 20;
-    Spadrao.hp_atual = 20;
-    Spadrao.lv = 20;
-    Spadrao.xp = 0;
-    Spadrao.xp_next_lv = 100;
-    Spadrao.def = 10;
-    Spadrao.atk = 10;
-    Spadrao.carisma = 15;
-    Spadrao.inteligencia = 15;
-    Spadrao.percepcao = 15;
-    return Spadrao;
-}
-
-
-
-personagem cadastro_ficha(){
-    status n_status = padrao(n_status);
-    personagem jogador;
-    printf("digite o seu nome:");
-    cin >> jogador.nome;
-
-    printf("\n","digite o sua idade:");
-    cin >> jogador.idade;
-
-    printf("\n","digite o sua classe:");
-    cin >> jogador.classe;
-
-    printf("\n","digite o sua raça:");
-    cin >> jogador.raca;
-
-    printf("\n","digite sua historia:");
-    getline(cin,jogador.descricao);
-
-    printf("\n","digite o seu nome:");
-    cin >> jogador.sexo;
-
-    while (jogador.sexo != "f" and jogador.sexo != "F" and jogador.sexo != "m" and jogador.sexo != "M" and jogador.sexo != "n" and jogador.sexo != "N")
-    {
-        cin >> jogador.sexo;
-    }
-    jogador.persona_s = n_status;
-    
-
-    return jogador;
-}
-
-
 
 int menu(){
     int tela = 0;
---
+++ vetor.hpp
@@ -0,0 +1,27 @@
+#ifndef VETOR_HPP
+#define VERTO_HPP
+
+#include <iostream>
+#include "pstatus.hpp"
+
+using namespace std;
+
+extern personagem* jogador; //extern = busca a variavel fora do arquivo ; personagem* atribui o endereço da memoria "jogador"
+extern int n_jogadores;//busca o elemento n_jogadores dora do arquivo;
+
+void add(int elements){
+
+}
+
+void remove(int element){
+
+}
+
+void modtfy(int element){
+
+}
+
+
+
+
+#endif
