d3ffa033ad13c4db4ba053c175ed2c9247fb4a29
FabioJuan <judoca041@gmail.com>
2026-02-10 17:24:49 -0300

    comentarios para ajustes em vetor

--- main.cpp
+++ main.cpp
@@ -12,29 +12,39 @@ int menu(){
     cout <<"-----------------criador de ficha de rpg-----------------------"<<"\n";
     cout <<"---------------------------------------------------------------"<<"\n";
     cout <<"(1)adicionar ficha"<<"\n";
-    cout <<"(2)retirar ficha"<<"\n";
+    cout <<"(2)remover ficha"<<"\n";
     cout <<"(3)buscar jogador"<<"\n";
     cout <<"(4)listar jogares"<<"\n";
-    cin >> tela;
-    switch (tela)
-    {
-        case 1:
-        
-        break;
-        
-        default:
-        break;
-    }
-    return tela;
 }
 int main(){
     int n_jogadores = 1;
+    int position  = 0;
+    int tela = 0;
     personagem *jogadores = new personagem[n_jogadores];// vetor dimanico 
-    Vetor personagens = Vetor(jogadores, n_jogadores);//o array jogadores recebe uma classe 
-    
+    Vetor personagens = Vetor(jogadores, n_jogadores, position);//o array jogadores recebe uma classe 
     personagens.imprime_jogadores();
-
     menu();
+
+    cin >> tela;
+    switch (tela)
+    {
+    case 1: 
+        personagens.add_lista();
+        break;
+    case 2:
+        personagens.remove();
+        break;
+    case 3:
+        break;
+    case 4 :
+        personagens.imprime_jogadores();
+        break;
+    default:
+
+        break;
+    }
+
+
 
     return 0;
 }
--- vetor.hpp
+++ vetor.hpp
@@ -10,11 +10,33 @@ class Vetor {
     private:
     personagem* m_personagens;//parametros do array 
     int m_tamanho;//parametro do tamanho
+    int position;
 
+    void le_arquivo(){
+        //le o arquivo principal de todos os personagens
+    }
+    void backup(){
+        //escreve em um outro arquivo todos os personagens no arquivo principal e backup
+    }
     public:
-    Vetor::Vetor(personagem* personagens, int tamanho): m_personagens(personagens), m_tamanho(tamanho) {}
+    Vetor::Vetor(personagem* personagens, int tamanho, int pos): m_personagens(personagens), m_tamanho(tamanho), position(pos) {}
     
 
+
+    status padrao(status Spadrao){
+        //status padrão de um novo personagem level 1 
+        Spadrao.hp = 20;
+        Spadrao.hp_atual = 20;
+        Spadrao.lv = 20;
+        Spadrao.xp = 0;
+        Spadrao.xp_next_lv = 100;
+        Spadrao.def = 10;
+        Spadrao.atk = 10;
+        Spadrao.carisma = 15;
+        Spadrao.inteligencia = 15;
+        Spadrao.percepcao = 15;
+        return Spadrao;
+    }
     personagem cadasdastro(){
         personagem novo_jogador;
         printf("digite o seu nome:");
@@ -35,6 +57,8 @@ class Vetor {
         printf("\n","digite o seu nome:");
         cin >> novo_jogador.sexo;
 
+        padrao(novo_jogador.persona_s);
+
         while (novo_jogador.sexo != "f" and novo_jogador.sexo != "F" and 
                 novo_jogador.sexo != "m" and novo_jogador.sexo != "M" and 
                 novo_jogador.sexo != "n" and novo_jogador.sexo != "N")
@@ -46,7 +70,10 @@ class Vetor {
 
     }
     void add_lista(){
-        cadasdastro();
+        personagem novo_personagem = cadasdastro();
+        //fazer o vetor aumentar de tamanho caso ele n tenha mais espaço se não somente colocar na proxima posição
+        position++;
+
     }
     void imprime_jogadores() {
         cout << "#id|#" << " #nome#"<<" #clase#"<<" #raça#"<<" #sexo#"<<" #idade#"<<" #descrição#";
@@ -57,8 +84,19 @@ class Vetor {
         }
     }
 
+    int ultimo_elemento(){
+        int len = 0;        
+        for(int i = 0;i <= m_tamanho;i++){
+            len++;                
+        }
+        return len;
+    }
 
-    void remove(int id) {
+
+    void remove() {
+        imprime_jogadores();
+    //criar a função que remove um personagem pelo id 
+        position--;
     } 
 
 };
